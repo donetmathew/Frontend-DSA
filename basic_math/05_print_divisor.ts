5617801019ef2f91f4de9be56200db18ec5dd6a9
donetmathew <56916905+donetmathew@users.noreply.github.com>
2024-02-27 17:00:44 -0500

    Create 05_print_divisor.ts

---
+++ basic_math/05_print_divisor.ts
@@ -0,0 +1,17 @@
+//problem-5 print divisor logic
+//Brute force -loop through from 1 to n and print.
+//Optimized- loop through 1 to sqrt(n) and print.
+function printDivisor(num) {
+  for (let i = 1; i < Math.sqrt(num); i++) {
+    if (num % i === 0) {
+      //  console.log(i);
+      //(Uncomment to see values)
+      if (num / i !== i) {
+        // added this condn to avoid printing 6 twice if num is 36
+        // console.log(num / i);
+        //(Uncomment to see values)
+      }
+    }
+  }
+}
+printDivisor(36);
