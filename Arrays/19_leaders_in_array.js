9da483054759c60f682bce771eb4f22731a03460
donetmathew <56916905+donetmathew@users.noreply.github.com>
2024-04-01 08:47:56 -0400

    Update 19_leaders_in_array.js

--- Arrays/19_leaders_in_array.js
+++ Arrays/19_leaders_in_array.js
@@ -1,32 +1,32 @@
-function leadersArrayBrute(arr){
-let result=[];
-	for(i=0;i<arr.length;i++){
-  	let leaders=true;
-    for(j=i;j<arr.length;j++){
-    	if(arr[i] < arr[j]){
-      	leaders=false;
-        //break;
-      }
+function leadersArrayBrute(arr) {
+    let result = [];
+    for (i = 0; i < arr.length; i++) {
+        let leaders = true;
+        for (j = i; j < arr.length; j++) {
+            if (arr[i] < arr[j]) {
+                leaders = false;
+                //break;
+            }
+        }
+        if (leaders) {
+            result.push(arr[i]);
+        }
     }
-    if(leaders){
-    	result.push(arr[i]);
-    }
-  }
-  return result;
+    return result;
 }
 
-console.log("Leaders in an array",leadersArrayBrute([10,22,12,3,0,6]))
+console.log("Leaders in an array", leadersArrayBrute([10, 22, 12, 3, 0, 6]))
 
-function leadersArrayOptimal(arr){
-  let max=Number.MIN_SAFE_INTEGER;
-  let result=[];
-  for(i=arr.length-1;i>=0;i--){
-		if(arr[i] > max){
-     max=arr[i];
-     result.push(arr[i])
+function leadersArrayOptimal(arr) {
+    let max = Number.MIN_SAFE_INTEGER;
+    let result = [];
+    for (i = arr.length - 1; i >= 0; i--) {
+        if (arr[i] > max) {
+            max = arr[i];
+            result.push(arr[i])
+        }
     }
-  }
-  return result;
+    return result;
 }
 
 console.log("Leaders in an array",leadersArrayOptimal([10,22,12,3,0,6]))
